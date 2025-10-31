9655e42c2a07151c1d741b3560953c36d32eac9c
Gabriel Barbosa <gabriel@greatquestion.co>
2025-10-31 10:22:46 -0300

    Remove unecessary skip

--- lib/ruby_snowflake/streaming_result.rb
+++ lib/ruby_snowflake/streaming_result.rb
@@ -28,8 +28,6 @@ module RubySnowflake
           data[index] = data[index].value # wait for it to finish
         end
 
-        next if data[index].nil?
-
         data[index].each do |row|
           yield wrap_row(row)
         end
